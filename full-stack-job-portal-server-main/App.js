8bad515f7f15d106ac3b05bf6ddf9689ee8d29bb
Priya Chauhan <Priya@123>
2025-11-07 02:08:41 +0530

    add url

--- full-stack-job-portal-server-main/App.js
+++ full-stack-job-portal-server-main/App.js
@@ -13,7 +13,6 @@ app.use(
   cors({
     origin: [
       "https://job-easy-vercel-project-d4dg.vercel.app",
-      "https://job-easy-vercel-project-d4dg.vercel.app/",
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:8000",
